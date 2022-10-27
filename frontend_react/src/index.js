38dde397ef1697d20d608fb485454e92fe729a42
Gauri Deshpande <gauri.deshpande22@rediffmail.com>
2022-10-27 22:35:41 +0530

    First commit

---
+++ frontend_react/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom'; // Through which we are going to connect to our HTML file.
+import App from './App';
+import './index.css';
+
+ReactDOM.render(<App/>,document.getElementById('root'));
